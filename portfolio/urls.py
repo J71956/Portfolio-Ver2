ad23bbd2ea23ae7a84962f2197d2374b11e5b0c8
Siu Yau Shing <90333201+J71956@users.noreply.github.com>
2025-01-06 16:58:07 +0800

    aaaaaaaa

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,7 +3,7 @@ from portfolio.views import index, home, projects, contact
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
     path('projects/', views.projects, name='projects'),
     path('contact/', views.contact, name='contact'),
     path('', index),

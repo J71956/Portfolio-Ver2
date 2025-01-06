f77f9dd9f3b490052230807e2d16b3d4357147eb
Siu Yau Shing <90333201+J71956@users.noreply.github.com>
2025-01-06 16:54:14 +0800

    cvxcvxcvxcvxcv

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from portfolio.views import index, home, projects, contact
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.projects, name='projects'),
     path('contact/', views.contact, name='contact'),
+    path('', index),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-
 # Create your views here.
 
 from django.http import HttpResponse
 from django.templatetags.static import static
+
+
+from datetime import datetime
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
 
 def home(request):
     html_content = """
